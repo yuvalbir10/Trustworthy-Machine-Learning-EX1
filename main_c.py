3363717de5fad9710bcf979785892d3956c63a4f
yuvalbir10 <yuvalbir10@gmail.com>
2024-06-22 23:21:57 +0300

    add first implementation to main_c.py. I think there might be a bug in the flip_bit function because for some reason only the 31-th bit makes a significant change in the RAD

--- main_c.py
+++ main_c.py
@@ -7,6 +7,21 @@ import random
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
+import struct
+
+def flip_bit(float_num, index):
+    # Convert float to its 32-bit representation
+    float_bytes = struct.pack('f', float_num)
+    float_int = struct.unpack('I', float_bytes)[0]
+
+    # Flip the i-th bit
+    flipped_int = float_int ^ (1 << index)
+
+    # Convert back to float
+    flipped_bytes = struct.pack('I', flipped_int)
+    flipped_float = struct.unpack('f', flipped_bytes)[0]
+
+    return flipped_float
 
 sns.set_theme()
 
@@ -45,13 +60,11 @@ for layer_name in layers:
         for _ in range(consts.BF_PER_LAYER):
             # randomly pick a weight
             weight_idx = random.randint(0, len(W.view(-1)) - 1)
-            weight = W.view(-1)[weight_idx]
+            weight = W.view(-1)[weight_idx].item()
             # randomly pick a bit to flip
             bit_idx = random.randint(0, 31)
-            # flip the bit
-            flipped_weight = int(weight) ^ (1 << bit_idx)
             # update the weight with the flipped bit
-            W.view(-1)[weight_idx] = flipped_weight
+            W.view(-1)[weight_idx] = flip_bit(weight, bit_idx)
             # compute the accuracy after bit flipping
             acc_bf = utils.compute_accuracy(model, data_loader, device)
             # compute the RAD
@@ -63,11 +76,11 @@ for layer_name in layers:
             # append the RAD to the overall RADs
             RADs_all.append(rad)
 
-# Max and % RAD>10%
+# Max and % RAD>15%
 RADs_all = np.array(RADs_all)
 print(f'Total # weights flipped: {len(RADs_all)}')
 print(f'Max RAD: {np.max(RADs_all):0.4f}')
-print(f'RAD>10%: {np.sum(RADs_all > 0.1) / RADs_all.size:0.4f}')
+print(f'RAD>15%: {np.sum(RADs_all > 0.15) / RADs_all.size:0.4f}')
 
 # boxplots: bit-flip index vs. RAD
 plt.figure()
